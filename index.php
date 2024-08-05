854a7c7edc4619204f82e838a19ba8d63f336afe
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-08-05 13:25:48 -0300

    update
    criei um arquivo css e melhorei a apresentação da CRUD

--- index.php
+++ index.php
@@ -5,27 +5,46 @@ require 'dao/produtoDaoMysql.php';
 
 $produtoDao = new ProdutoDaoMysql($pdo);
 $lista = $produtoDao->findAll();
+
 ?>
 
-<link rel="stylesheet" href="style.css">
-<a href="adicionar.php">ADICIONAR PRODUTO</a> <table border="1" width="100%">
-    <tr>
-        <th>ID</th>
-        <th>NOME DO PRODUTO</th>
-        <th>VALOR UNITÁRIO</th>
-        <th>AÇÕES</th>
-    </tr>
-
-    <?php foreach ($lista as $produto) : ?>
-        <tr>
-            <td><?= $produto->getId(); ?></td>
-            <td><?= $produto->getNome(); ?></td>
-            <td><?= $produto->getValor(); ?></td>
-            <td>
-                <a href="editar.php?id=<?= $produto->getId(); ?>"> [Editar] </a>
-                <a href="excluir.php?id=<?= $produto->getId(); ?>" onclick="return confirm('Tem certeza da sua ação?')"> [Excluir] </a>
-            </td>
-        </tr>
-    <?php endforeach ?>
-</table>
-
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Produtos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Lista de Produtos do mercadinho</h1>
+            <a href="adicionar.php" class="button">ADICIONAR PRODUTO</a>
+        </header>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NOME DO PRODUTO</th>
+                    <th>VALOR UNITÁRIO</th>
+                    <th>AÇÕES</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($lista as $produto) : ?>
+                    <tr>
+                        <td><?= $produto->getId(); ?></td>
+                        <td><?= $produto->getNome(); ?></td>
+                        <td><?= $produto->getValor(); ?></td>
+                        <td>
+                            <a href="editar.php?id=<?= $produto->getId(); ?>" class="edit">Editar</a>
+                            <a href="excluir.php?id=<?= $produto->getId(); ?>" class="delete" onclick="return confirm('Tem certeza da sua ação?')">Excluir</a>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
